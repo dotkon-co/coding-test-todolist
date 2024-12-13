c5913a3b35062b05e0ea3d9efc9188b52c698720
Kako Ferrare <kakoferrare87@gmail.com>
2024-12-13 09:30:10 -0300

    feat: added service project

---
+++ ToDo/Service/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+using Domain.Requests.Register;
+
+namespace Service.Services
+{
+	public class UserService : IUserService
+	{
+		public Task<bool> DeleteAsync(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<UserEntity>> GetAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<UserEntity> RegisterAsync(RegisterRequest user)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
